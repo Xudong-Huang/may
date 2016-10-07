257799ea61c52cdbf7a77a4bfec188bc9decce2f
Xudong Huang <huangxu008@hotmail.com>
2016-10-07 20:33:41 +0800

    add timeout for udp socket

--- src/net/udp.rs
+++ src/net/udp.rs
@@ -1,16 +1,39 @@
-// use io::net as net_impl;
+use io::net as net_impl;
 use std::io;
+use std::time::Duration;
 use std::net::{self, ToSocketAddrs, SocketAddr, Ipv4Addr, Ipv6Addr};
+use yield_now::yield_with;
+use coroutine::is_coroutine;
 
 #[derive(Debug)]
 pub struct UdpSocket {
     sys: net::UdpSocket,
+    read_timeout: Option<Duration>,
+    write_timeout: Option<Duration>,
 }
 
 impl UdpSocket {
-    /// Creates a UDP socket from the given address.
+    pub fn new(s: net::UdpSocket) -> io::Result<UdpSocket> {
+        // only set non blocking in coroutine context
+        // we would first call nonblocking io in the coroutine
+        // to avoid unnecessary context switch
+        // try!(s.set_nonblocking(true));
+
+        net_impl::add_socket(&s).map(|_| {
+            UdpSocket {
+                sys: s,
+                read_timeout: None,
+                write_timeout: None,
+            }
+        })
+    }
+
+    pub fn inner(&self) -> &net::UdpSocket {
+        &self.sys
+    }
+
     pub fn bind<A: ToSocketAddrs>(addr: A) -> io::Result<UdpSocket> {
-        net::UdpSocket::bind(addr).map(|s| UdpSocket { sys: s })
+        net::UdpSocket::bind(addr).and_then(|s| UdpSocket::new(s))
     }
 
     pub fn connect<A: ToSocketAddrs>(&self, addr: A) -> io::Result<()> {
@@ -23,9 +46,13 @@ impl UdpSocket {
     }
 
     pub fn try_clone(&self) -> io::Result<UdpSocket> {
-        self.sys
-            .try_clone()
-            .map(From::from)
+        self.sys.try_clone().map(|s| {
+            UdpSocket {
+                sys: s,
+                read_timeout: self.read_timeout,
+                write_timeout: self.read_timeout,
+            }
+        })
     }
 
     pub fn send_to<A: ToSocketAddrs>(&self, buf: &[u8], addr: A) -> io::Result<usize> {
@@ -34,6 +61,29 @@ impl UdpSocket {
 
     pub fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
         self.sys.recv_from(buf)
+        // if !is_coroutine() {
+        //     // this can't be nonblocking!!
+        //     try!(self.sys.set_nonblocking(false));
+        //     let ret = try!(self.sys.recv_from(buf));
+        //     try!(self.sys.set_nonblocking(true));;
+        //     return Ok(ret);
+        // }
+
+        // this is an earlier return try for nonblocking read
+        // match self.sys.recv_from(buf) {
+        //     Err(err) => {
+        //         if err.kind() != io::ErrorKind::WouldBlock {
+        //             return Err(err);
+        //         }
+        //     }
+        //     ret @ Ok(..) => {
+        //         return ret;
+        //     }
+        // }
+
+        // let reader = net_impl::UdpRecvFrom::new(self, buf);
+        // yield_with(&reader);
+        // reader.done()
     }
 
     pub fn send(&self, buf: &[u8]) -> io::Result<usize> {
@@ -42,6 +92,26 @@ impl UdpSocket {
 
     pub fn recv(&self, buf: &mut [u8]) -> io::Result<usize> {
         self.sys.recv(buf)
+    }
+
+    pub fn set_read_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        let me = unsafe { &mut *(self as *const _ as *mut Self) };
+        me.read_timeout = dur;
+        Ok(())
+    }
+
+    pub fn set_write_timeout(&self, dur: Option<Duration>) -> io::Result<()> {
+        let me = unsafe { &mut *(self as *const _ as *mut Self) };
+        me.write_timeout = dur;
+        Ok(())
+    }
+
+    pub fn read_timeout(&self) -> io::Result<Option<Duration>> {
+        Ok(self.read_timeout)
+    }
+
+    pub fn write_timeout(&self) -> io::Result<Option<Duration>> {
+        Ok(self.write_timeout)
     }
 
     pub fn broadcast(&self) -> io::Result<bool> {
@@ -102,12 +172,6 @@ impl UdpSocket {
 
     pub fn take_error(&self) -> io::Result<Option<io::Error>> {
         self.sys.take_error()
-    }
-}
-
-impl From<net::UdpSocket> for UdpSocket {
-    fn from(sys: net::UdpSocket) -> UdpSocket {
-        UdpSocket { sys: sys }
     }
 }
 
