c1972c2463c192bc0c726e0609772b986800769f
Xudong Huang <huangxu008@hotmail.com>
2016-10-08 20:37:48 +0800

    add unix udp recv_from

--- src/io/sys/unix/net/mod.rs
+++ src/io/sys/unix/net/mod.rs
@@ -7,7 +7,7 @@ mod socket_write;
 // mod tcp_stream_connect;
 // mod tcp_listener_accpet;
 // mod udp_send_to;
-// mod udp_recv_from;
+mod udp_recv_from;
 
 pub use self::socket_read::SocketRead;
 pub use self::socket_write::SocketWrite;
@@ -15,7 +15,7 @@ pub use self::socket_write::SocketWrite;
 // pub use self::tcp_listener_accpet::TcpListenerAccept;
 //
 // pub use self::udp_send_to::UdpSendTo;
-// pub use self::udp_recv_from::UdpRecvFrom;
+pub use self::udp_recv_from::UdpRecvFrom;
 
 #[inline]
 pub fn add_socket<T: AsRawFd + ?Sized>(t: &T) -> io::Result<()> {
--- src/io/sys/unix/net/udp_recv_from.rs
+++ src/io/sys/unix/net/udp_recv_from.rs
@@ -1,13 +1,12 @@
-use std;
-use std::io;
-use std::net::SocketAddr;
+use std::{self, io};
 use std::time::Duration;
-use std::os::windows::io::AsRawSocket;
+use std::net::SocketAddr;
+use std::os::unix::io::{AsRawFd, RawFd};
 use super::co_io_result;
-use super::super::EventData;
-use super::super::winapi::*;
-use super::super::miow::net::{UdpSocketExt, SocketAddrBuf};
+use super::super::from_nix_error;
+use super::super::{EventData, FLAG_READ};
 use net::UdpSocket;
+use yield_now::yield_with;
 use scheduler::get_scheduler;
 use coroutine::{CoroutineImpl, EventSource};
 
@@ -15,30 +14,37 @@ pub struct UdpRecvFrom<'a> {
     io_data: EventData,
     buf: &'a mut [u8],
     socket: &'a std::net::UdpSocket,
-    addr: SocketAddrBuf,
     timeout: Option<Duration>,
 }
 
 impl<'a> UdpRecvFrom<'a> {
     pub fn new(socket: &'a UdpSocket, buf: &'a mut [u8]) -> Self {
         UdpRecvFrom {
-            io_data: EventData::new(socket.as_raw_socket() as HANDLE),
+            io_data: EventData::new(socket.as_raw_fd(), FLAG_READ),
             buf: buf,
             socket: socket.inner(),
-            addr: SocketAddrBuf::new(),
             timeout: socket.read_timeout().unwrap(),
         }
     }
 
     #[inline]
     pub fn done(self) -> io::Result<(usize, SocketAddr)> {
-        let size = try!(co_io_result(&self.io_data));
-        let addr = try!(self.addr
-            .to_socket_addr()
-            .ok_or_else(|| {
-                io::Error::new(io::ErrorKind::Other, "could not obtain remote address")
-            }));
-        Ok((size, addr))
+        loop {
+            try!(co_io_result());
+            match self.socket.recv_from(self.buf) {
+                Err(err) => {
+                    if err.kind() != io::ErrorKind::WouldBlock {
+                        return Err(err);
+                    }
+                }
+                ret @ Ok(..) => {
+                    return ret;
+                }
+            }
+
+            // the result is still WouldBlock, need to try again
+            yield_with(&self);
+        }
     }
 }
 
@@ -47,19 +53,6 @@ impl<'a> EventSource for UdpRecvFrom<'a> {
         let s = get_scheduler();
         // prepare the co first
         self.io_data.co = Some(co);
-        // call the overlapped read API
-        let ret = co_try!(s, self.io_data.co.take().unwrap(), unsafe {
-            self.socket
-                .recv_from_overlapped(self.buf, &mut self.addr, self.io_data.get_overlapped())
-        });
-
-        // the operation is success, we no need to wait any more
-        if ret == true {
-            // the done function would contain the actual io size
-            // just let the iocp schedule the coroutine
-            // s.schedule_io(co);
-            return;
-        }
 
         // register the io operaton
         co_try!(s,
