71c57060ab68fc8262cc7fbeb7ab3a10950faa86
Xudong Huang <huangxu008@hotmail.com>
2016-10-09 22:11:30 +0800

    fix unix timer handler logic

--- src/io/sys/unix/mod.rs
+++ src/io/sys/unix/mod.rs
@@ -7,7 +7,9 @@ mod select;
 
 pub mod net;
 
+use std::io;
 use std::os::unix::io::RawFd;
+use yield_now::set_co_para;
 use scheduler::get_scheduler;
 use coroutine::CoroutineImpl;
 use timeout_list::TimeoutHandle;
@@ -37,9 +39,20 @@ pub fn timeout_handler(data: TimerData) {
     // remove the event timer
     event_data.timer.take();
 
-    let s = get_scheduler().get_selector();
+    let s = get_scheduler();
     // ignore the error, the select may grab the data first!
-    s.cancel_io(event_data).map_err(|e| error!("cancel io failed! e = {}", e)).ok();
+    s.get_selector().cancel_io(event_data).map_err(|e| error!("cancel io failed! e = {}", e)).ok();
+
+    let co = event_data.co.take();
+    if co.is_none() {
+        // there is no coroutine prepared, just ignore this one
+        warn!("can't get coroutine in the epoll select");
+        return;
+    }
+
+    let mut co = co.unwrap();
+    set_co_para(&mut co, io::Error::new(io::ErrorKind::TimedOut, "timeout"));
+    s.schedule_io(co);
 }
 
 type TimerHandle = TimeoutHandle<TimerData>;
