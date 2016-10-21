e98d5efc398f7533a6681f5073df79079868b672
Xudong Huang <huangxu008@hotmail.com>
2016-10-21 14:25:30 +0800

    enable unix timer wakeup

--- src/io/sys/unix/epoll.rs
+++ src/io/sys/unix/epoll.rs
@@ -1,11 +1,11 @@
 use std::time::Duration;
 use std::os::unix::io::RawFd;
 use std::{io, cmp, ptr, isize};
+use smallvec::SmallVec;
 use timeout_list::{now, ns_to_ms};
 use super::nix::sys::epoll::*;
-use super::nix::fcntl::FcntlArg::F_SETFL;
-use super::nix::fcntl::{fcntl, O_CLOEXEC};
-use super::nix::unistd::close;
+use super::nix::unistd::{read, write, close};
+use super::libc::{eventfd, EFD_NONBLOCK};
 use super::{EventFlags, FLAG_READ, FLAG_WRITE};
 use super::{EventData, TimerList, from_nix_error, timeout_handler};
 
@@ -25,29 +25,65 @@ fn interest_to_epoll_kind(interest: EventFlags) -> EpollEventKind {
     kind
 }
 
+fn create_eventfd() -> io::Result<RawFd> {
+    let fd = unsafe { eventfd(0, EFD_NONBLOCK) };
+    if fd < 0 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(fd as RawFd)
+}
+
 pub type SysEvent = EpollEvent;
 
+struct SingleSelector {
+    epfd: RawFd,
+    evfd: RawFd,
+    timer_list: TimerList,
+}
+
+impl SingleSelector {
+    pub fn new() -> io::Result<Self> {
+        let info = EpollEvent {
+            events: EpollEventKind::from(EPOLLONESHOT | EPOLLET | EPOLLIN),
+            data: 0, // wakeup data is 0
+        };
+
+        let epfd = try!(epoll_create().map_err(from_nix_error));
+        let evfd = try!(create_eventfd());
+
+        // add the eventfd to the epfd
+        try!(epoll_ctl(epfd, EpollOp::EpollCtlAdd, evfd, &info).map_err(from_nix_error));
+
+        Ok(SingleSelector {
+            epfd: epfd,
+            evfd: evfd,
+            timer_list: TimerList::new(),
+        })
+    }
+}
+
+impl Drop for SingleSelector {
+    fn drop(&mut self) {
+        let _ = close(self.evfd);
+        let _ = close(self.epfd);
+    }
+}
 
 pub struct Selector {
-    epfd: Vec<RawFd>,
-    timer_list: Vec<TimerList>,
+    // 128 should be fine for max io threads
+    vec: SmallVec<[SingleSelector; 128]>,
 }
 
 impl Selector {
-    pub fn new(io_workers: usize) -> io::Result<Selector> {
-        let mut epfd = vec![0; io_workers];
-        for i in 0..io_workers {
-            let fd = try!(epoll_create().map_err(from_nix_error));
-            try!(fcntl(fd, F_SETFL(O_CLOEXEC)).map_err(from_nix_error));
-            epfd[i] = fd;
+    pub fn new(io_workers: usize) -> io::Result<Self> {
+        let mut s = Selector { vec: SmallVec::new() };
+
+        for _ in 0..io_workers {
+            let ss = try!(SingleSelector::new());
+            s.vec.push(ss);
         }
 
-        let timer_list = (0..io_workers).map(|_| TimerList::new()).collect();
-
-        Ok(Selector {
-            epfd: epfd,
-            timer_list: timer_list,
-        })
+        Ok(s)
     }
 
     pub fn select(&self,
@@ -58,20 +94,20 @@ impl Selector {
         let timeout_ms = timeout.map(|to| cmp::min(ns_to_ms(to), isize::MAX as u64) as isize)
             .unwrap_or(-1);
         // info!("select; timeout={:?}", timeout_ms);
-        // info!("polling epoll");
 
         // Wait for epoll events for at most timeout_ms milliseconds
-        let n = try!(epoll_wait(self.epfd[id], events, timeout_ms).map_err(from_nix_error));
+        let n = try!(epoll_wait(self.vec[id].epfd, events, timeout_ms).map_err(from_nix_error));
 
         for event in events[..n].iter() {
             if event.data == 0 {
                 // this is just a wakeup event, ignore it
-                // error!("got null data event in select");
+                let mut buf = [0u8; 8];
+                // clear the eventfd, ignore the result
+                read(self.vec[id].evfd, &mut buf).ok();
+                // info!("got wakeup event in select");
                 continue;
             }
             let data = unsafe { &mut *(event.data as *mut EventData) };
-            // info!("select got event, data={:p}", data);
-
             let mut co = data.co.take().expect("can't get co in selector");
             co.prefetch();
 
@@ -93,14 +129,16 @@ impl Selector {
         }
 
         // deal with the timer list
-        let next_expire = self.timer_list[id].schedule_timer(now(), &timeout_handler);
+        let next_expire = self.vec[id].timer_list.schedule_timer(now(), &timeout_handler);
         Ok(next_expire)
     }
 
     // this will post an os event so that we can wakeup the event loop
     #[inline]
-    fn wakeup(&self, _id: usize) {
-        // self.port.post(CompletionStatus::new(0, 0, ptr::null_mut())).unwrap();
+    fn wakeup(&self, id: usize) {
+        let buf = unsafe { ::std::slice::from_raw_parts(&1u64 as *const u64 as _, 8) };
+        let ret = write(self.vec[id].evfd, buf);
+        info!("wakeup id={:?}, ret={:?}", id, ret);
     }
 
     // register io event to the selector
@@ -123,7 +161,8 @@ impl Selector {
             data: ev_data as *const _ as _,
         };
         let fd = ev_data.fd;
-        let epfd = self.epfd[fd as usize % self.epfd.len()];
+        let id = fd as usize % self.vec.len();
+        let epfd = self.vec[id].epfd;
         info!("mod fd to epoll select, fd={:?}", fd);
         epoll_ctl(epfd, EpollOp::EpollCtlAdd, fd, &info).map_err(from_nix_error)
     }
@@ -134,7 +173,8 @@ impl Selector {
             events: EpollEventKind::empty(),
             data: 0,
         };
-        let epfd = self.epfd[fd as usize % self.epfd.len()];
+        let id = fd as usize % self.vec.len();
+        let epfd = self.vec[id].epfd;
         info!("add fd to epoll select, fd={:?}", fd);
         epoll_ctl(epfd, EpollOp::EpollCtlDel, fd, &info).ok();
     }
@@ -142,23 +182,15 @@ impl Selector {
     // register the io request to the timeout list
     #[inline]
     pub fn add_io_timer(&self, io: &mut EventData, timeout: Option<Duration>) {
-        let id = io.fd as usize % self.epfd.len();
+        let id = io.fd as usize % self.vec.len();
         io.timer = timeout.map(|dur| {
             // info!("io timeout = {:?}", dur);
-            let (h, b_new) = self.timer_list[id].add_timer(dur, io.timer_data());
+            let (h, b_new) = self.vec[id].timer_list.add_timer(dur, io.timer_data());
             if b_new {
                 // wakeup the event loop threead to recal the next wait timeout
                 self.wakeup(id);
             }
             h
         });
-    }
-}
-
-impl Drop for Selector {
-    fn drop(&mut self) {
-        for fd in &self.epfd {
-            let _ = close(*fd);
-        }
     }
 }
