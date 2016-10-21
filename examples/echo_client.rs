33249e569cd2eb51ae37cb1987ebb2fbe9523b67
Xudong Huang <huangxu008@hotmail.com>
2016-10-21 19:51:06 +0800

    add arg parse for echo_client

--- examples/echo_client.rs
+++ examples/echo_client.rs
@@ -1,8 +1,42 @@
+extern crate rustc_serialize;
 extern crate coroutine;
+extern crate docopt;
+
 use std::time::Duration;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
+use std::net::ToSocketAddrs;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use coroutine::net::TcpStream;
+
+use docopt::Docopt;
+
+const VERSION: &'static str = "0.1.0";
+
+const USAGE: &'static str = "
+Tcp echo client.
+
+Usage:
+  echo_client [-c <connections>] [-t <time>] [-l <length>] -a <address>
+  echo_client (-h | --help)
+  echo_client (-v | --version)
+
+Options:
+  -h --help         Show this screen.
+  -v --version      Show version.
+  -l <length>       packet length in bytes [default: 100].
+  -c <connections>  concurent connections  [default: 100].
+  -t <time>         time to run in seconds [default: 10].
+  -a <address>      target address (e.g. 127.0.0.1:8080).
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_c: usize,
+    flag_a: String,
+    flag_l: usize,
+    flag_t: usize,
+    flag_v: bool,
+}
 
 macro_rules! t {
     ($e: expr) => (match $e {
@@ -12,22 +46,32 @@ macro_rules! t {
 }
 
 fn main() {
-    coroutine::scheduler_set_workers(1);
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
 
-    // let target_addr = "127.0.0.1:5555";
-    let target_addr = "127.0.0.1:8080";
-    // let target_addr = "192.168.56.102:5555";
-    // let target_addr = "127.0.0.1:3000";
-    let test_msg_len = 80;
-    let test_conn_num = 1000;
-    let test_seconds = 10;
+    if args.flag_v {
+        return println!("echo_client: {}", VERSION);
+    }
+
+    let target_addr: &str = &args.flag_a;
+    let test_msg_len = args.flag_l;
+    let test_conn_num = args.flag_c;
+    let test_seconds = args.flag_t;
     // let io_timeout = 2;
+
+    coroutine::scheduler_set_workers(1);
 
     let stop = AtomicBool::new(false);
     let in_num = AtomicUsize::new(0);
     let out_num = AtomicUsize::new(0);
 
     let msg = vec![0; test_msg_len];
+
+    let err = io::Error::new(io::ErrorKind::Other, "can't resolve socket addresses");
+    let addr = t!(target_addr.to_socket_addrs())
+        .fold(Err(err), |prev, addr| prev.or_else(|_| Ok(addr)))
+        .unwrap();
 
     coroutine::scope(|scope| {
         scope.spawn(|| {
@@ -37,7 +81,7 @@ fn main() {
 
         for _ in 0..test_conn_num {
             scope.spawn(|| {
-                let mut conn = t!(TcpStream::connect(target_addr));
+                let mut conn = t!(TcpStream::connect(addr));
                 // t!(conn.set_read_timeout(Some(Duration::from_secs(io_timeout))));
                 // t!(conn.set_write_timeout(Some(Duration::from_secs(io_timeout))));
                 t!(conn.set_nodelay(true));
