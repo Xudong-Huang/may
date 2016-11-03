1ec2de47b0c4f7b69c25a06c77e2da2d2f318604
Xudong Huang <huangxu008@hotmail.com>
2016-11-03 11:51:23 +0800

    refine park logic

--- src/park.rs
+++ src/park.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use std::time::Duration;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use sync::BoxedOption;
 use scheduler::get_scheduler;
 use coroutine::{CoroutineImpl, EventSource};
@@ -8,8 +8,9 @@ use coroutine::{CoroutineImpl, EventSource};
 pub struct Park {
     // the coroutine that waiting for this join handler
     wait_co: Arc<BoxedOption<CoroutineImpl>>,
-    // when set means the Park no need to block
-    state: AtomicBool,
+    // when odd means the Park no need to block
+    // the low bit used as flag, and higher bits used as tag to prevent ABA problem
+    state: AtomicUsize,
     timeout: Option<Duration>,
 }
 
@@ -18,7 +19,7 @@ impl Park {
     pub fn new() -> Self {
         Park {
             wait_co: Arc::new(BoxedOption::none()),
-            state: AtomicBool::new(false),
+            state: AtomicUsize::new(0),
             timeout: None,
         }
     }
@@ -33,50 +34,45 @@ impl Park {
     // when the state is true, we clear it and indicate not to block
     // when the state is false, means we need real park
     pub fn check_park(&self) -> bool {
-        if !self.state.load(Ordering::Relaxed) {
+        let mut state = self.state.load(Ordering::Acquire);
+        if state & 1 == 0 {
             return true;
         }
 
         loop {
             match self.state
-                .compare_exchange_weak(true, false, Ordering::Relaxed, Ordering::Relaxed) {
-                Ok(_) => {
-                    // successfully consume the state
-                    // don't need to block
-                    return false;
-                }
-                Err(x) => {
-                    if !x {
-                        return true;
-                    }
-                }
+                .compare_exchange_weak(state, state + 1, Ordering::AcqRel, Ordering::Relaxed) {
+                Ok(_) => return false, // successfully consume the state, no need to block
+                Err(x) if x & 1 == 0 => return true,
+                Err(y) => state = y,
             }
         }
     }
 
     // unpark the underlying coroutine if any
     pub fn unpark(&self) {
-        if self.state.load(Ordering::Relaxed) {
+        let mut state = self.state.load(Ordering::Acquire);
+        if state & 1 == 1 {
             // the state is already set do nothing here
             return;
         }
 
         loop {
             match self.state
-                .compare_exchange_weak(false, true, Ordering::Relaxed, Ordering::Relaxed) {
-                Ok(_) => {
-                    self.wait_co.take_fast(Ordering::Acquire).map(|co| {
-                        get_scheduler().schedule(co);
-                    });
-                    return;
-                }
-                Err(x) => {
-                    if x {
-                        break; // already set, do nothing
-                    }
-                }
+                .compare_exchange_weak(state, state + 1, Ordering::AcqRel, Ordering::Relaxed) {
+                Ok(_) => return self.wake_up(),
+                Err(x) if x & 1 == 1 => break, // already set, do nothing
+                Err(y) => state = y,
             }
         }
+    }
+
+    #[inline]
+    fn wake_up(&self) {
+        // ::std::sync::atomic::fence(Ordering::SeqCst);
+        self.wait_co
+            .take_fast(Ordering::Acquire)
+            .map(|co| get_scheduler().schedule(co));
     }
 }
 
@@ -84,18 +80,14 @@ impl EventSource for Park {
     // register the coroutine to the park
     fn subscribe(&mut self, co: CoroutineImpl) {
         let s = get_scheduler();
-        let co = Arc::new(BoxedOption::some(co));
-        self.timeout.take().map(|dur| {
-            s.add_timer(dur, co.clone());
-        });
-
         // register the coroutine
-        self.wait_co = co;
-        // re-check the state
-        if !self.check_park() {
-            self.wait_co.take_fast(Ordering::Acquire).map(|co| {
-                s.schedule(co);
-            });
+        self.wait_co.swap(co, Ordering::Release);
+        let co = self.wait_co.clone();
+        self.timeout.take().map(|dur| s.add_timer(dur, co));
+
+        // re-check the state, only clear once after resume
+        if self.state.load(Ordering::Acquire) & 1 == 1 {
+            self.wake_up();
         }
     }
 }
