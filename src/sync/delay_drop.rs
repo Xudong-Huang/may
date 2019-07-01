720b46f4e24cdf11eeaa66c8bc8fed6a7c144eb7
Xudong Huang <huangxu008@hotmail.com>
2019-07-01 09:51:15 +0800

    :pencil: refine delay drop impl

--- src/sync/delay_drop.rs
+++ src/sync/delay_drop.rs
@@ -1,57 +1,48 @@
 use crate::yield_now::yield_now;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct DropGuard<'a>(&'a DelayDrop);
 pub struct DelayDrop {
     // can't alloc in stack because it may moved
-    // can_drop.0 is the flag that when kernel is done
-    // can_drop.1 is the flag that when kernel is started
-    can_drop: Box<(AtomicBool, AtomicBool)>,
+    // can_drop & 0x1 is the flag that when kernel is done
+    // can_drop & 0x2 is the flag that when kernel is started
+    can_drop: AtomicUsize,
 }
 
 impl DelayDrop {
     pub fn new() -> Self {
         DelayDrop {
-            can_drop: Box::new((AtomicBool::new(false), AtomicBool::new(false))),
+            can_drop: AtomicUsize::new(0),
         }
     }
 
     pub fn delay_drop(&self) -> DropGuard {
-        self.can_drop.1.store(true, Ordering::Release);
+        self.can_drop.store(2, Ordering::Release);
         DropGuard(self)
     }
 
-    // windows platform doesn't use this method
-    #[allow(dead_code)]
     pub fn reset(&self) {
         // wait the kernel finished
-        if !self.can_drop.1.load(Ordering::Acquire) {
-            while !self.can_drop.0.load(Ordering::Acquire) {
+        if self.can_drop.load(Ordering::Acquire) & 2 != 0 {
+            while self.can_drop.load(Ordering::Acquire) == 2 {
                 yield_now();
             }
         }
 
-        self.can_drop.0.store(false, Ordering::Release);
-        self.can_drop.1.store(false, Ordering::Release);
+        self.can_drop.store(0, Ordering::Release);
     }
 }
 
 impl<'a> Drop for DropGuard<'a> {
     fn drop(&mut self) {
         // kernel would set it to true
-        self.0.can_drop.0.store(true, Ordering::Release);
+        self.0.can_drop.fetch_and(1, Ordering::Release);
     }
 }
 
 impl Drop for DelayDrop {
     fn drop(&mut self) {
         // wait for drop
-        if !self.can_drop.1.load(Ordering::Acquire) {
-            return;
-        }
-
-        while !self.can_drop.0.load(Ordering::Acquire) {
-            yield_now();
-        }
+        self.reset()
     }
 }
