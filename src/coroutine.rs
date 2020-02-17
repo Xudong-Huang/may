625af3c95ed119053c6f0ea1bfbcbde35f454ba5
Xudong Huang <huangxu008@hotmail.com>
2020-02-17 21:40:19 +0800

    :pencil: public Coroutine

--- src/coroutine.rs
+++ src/coroutine.rs
@@ -1,6 +1,8 @@
 // re-export coroutine interface
 pub use crate::cancel::trigger_cancel_panic;
-pub use crate::coroutine_impl::{current, is_coroutine, park, park_timeout, spawn, Builder};
+pub use crate::coroutine_impl::{
+    current, is_coroutine, park, park_timeout, spawn, Builder, Coroutine,
+};
 pub use crate::join::JoinHandle;
 pub use crate::park::ParkError;
 pub use crate::scoped::scope;
