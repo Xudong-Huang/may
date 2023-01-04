db4fbfd4172ba5f0dbf88060df16719490c8efd8
Xudong Huang <huangxu008@hotmail.com>
2023-01-04 17:01:28 +0800

    :pencil: refine bench code

--- benches/lib.rs
+++ benches/lib.rs
@@ -14,11 +14,13 @@ fn yield_bench(b: &mut Bencher) {
     // when cancel the generator
     // panic::set_hook(Box::new(|_| {}));
     b.iter(|| {
-        let h = go!(|| for _i in 0..10000 {
-            yield_now();
+        scope(|s| {
+            for _ in 0..1000 {
+                go!(s, || for _i in 0..1000 {
+                    yield_now();
+                });
+            }
         });
-
-        h.join().unwrap();
     });
 }
 
@@ -27,22 +29,19 @@ fn spawn_bench(b: &mut Bencher) {
     b.iter(|| {
         let total_work = 1000;
         let threads = 2;
-        let mut vec = Vec::with_capacity(threads);
-        for _t in 0..threads {
-            let j = std::thread::spawn(move || {
-                scope(|scope| {
-                    for _i in 0..total_work / threads {
-                        go!(scope, || {
-                            // yield_now();
-                        });
-                    }
+        std::thread::scope(|s| {
+            for _t in 0..threads {
+                s.spawn(move || {
+                    scope(|scope| {
+                        for _i in 0..total_work / threads {
+                            go!(scope, || {
+                                // yield_now();
+                            });
+                        }
+                    });
                 });
-            });
-            vec.push(j);
-        }
-        for j in vec {
-            j.join().unwrap();
-        }
+            }
+        });
     });
 }
 
@@ -52,20 +51,17 @@ fn spawn_bench_1(b: &mut Bencher) {
     b.iter(|| {
         let total_work = 1000;
         let threads = 2;
-        let mut vec = Vec::with_capacity(threads);
-        for _t in 0..threads {
-            let work = total_work / threads;
-            let j = std::thread::spawn(move || {
-                let v = (0..work).map(|_| go!(|| {})).collect::<Vec<_>>();
-                for h in v {
-                    h.join().unwrap();
-                }
-            });
-            vec.push(j);
-        }
-        for j in vec {
-            j.join().unwrap();
-        }
+        std::thread::scope(|s| {
+            for _t in 0..threads {
+                let work = total_work / threads;
+                s.spawn(move || {
+                    let v = (0..work).map(|_| go!(|| {})).collect::<Vec<_>>();
+                    for h in v {
+                        h.join().unwrap();
+                    }
+                });
+            }
+        });
     });
 }
 
@@ -74,22 +70,19 @@ fn smoke_bench(b: &mut Bencher) {
     may::config().set_pool_capacity(10000);
     b.iter(|| {
         let threads = 5;
-        let mut vec = Vec::with_capacity(threads);
-        for _t in 0..threads {
-            let j = std::thread::spawn(|| {
-                scope(|scope| {
-                    for _i in 0..200 {
-                        go!(scope, || for _j in 0..1000 {
-                            yield_now();
-                        });
-                    }
+        std::thread::scope(|s| {
+            for _t in 0..threads {
+                s.spawn(|| {
+                    scope(|scope| {
+                        for _i in 0..200 {
+                            go!(scope, || for _j in 0..1000 {
+                                yield_now();
+                            });
+                        }
+                    });
                 });
-            });
-            vec.push(j);
-        }
-        for j in vec {
-            j.join().unwrap();
-        }
+            }
+        });
     });
 }
 
@@ -98,22 +91,19 @@ fn smoke_bench_1(b: &mut Bencher) {
     may::config().set_pool_capacity(10000);
     b.iter(|| {
         let threads = 5;
-        let mut vec = Vec::with_capacity(threads);
-        for _t in 0..threads {
-            let j = std::thread::spawn(|| {
-                scope(|scope| {
-                    for _i in 0..2000 {
-                        go!(scope, || for _j in 0..4 {
-                            yield_now();
-                        });
-                    }
+        std::thread::scope(|s| {
+            for _t in 0..threads {
+                s.spawn(|| {
+                    scope(|scope| {
+                        for _i in 0..2000 {
+                            go!(scope, || for _j in 0..4 {
+                                yield_now();
+                            });
+                        }
+                    });
                 });
-            });
-            vec.push(j);
-        }
-        for j in vec {
-            j.join().unwrap();
-        }
+            }
+        });
     });
 }
 
