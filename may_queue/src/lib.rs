4ee5a1d3bb0a237b0fddac027520a35edb908ab7
Xudong Huang <huangxu008@hotmail.com>
2023-02-13 14:20:58 +0800

    :pencil: remove unused queues

--- may_queue/src/lib.rs
+++ may_queue/src/lib.rs
@@ -2,17 +2,11 @@
 
 mod atomic;
 
-pub mod mpsc_seg_queue;
-pub mod seg_queue;
-pub mod tokio_queue;
-
-pub mod mpmc_bounded;
 pub mod mpsc;
 pub mod mpsc_list;
 pub mod mpsc_list_v1;
 pub mod spmc;
 pub mod spsc;
-pub mod unordered_mpsc;
 
 #[cfg(test)]
 mod test_queue {
